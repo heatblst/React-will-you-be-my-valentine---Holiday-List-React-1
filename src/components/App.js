a795ee6dabbb8eca0163a188e8728bc68d5ee1d1
lucifer1799 <47712039+lucifer1799@users.noreply.github.com>
2021-02-15 13:56:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,35 +12,19 @@ class App extends Component {
     { name: 'Darjeeling', country: 'India' },
     { name: 'Tokyo', country: 'Japan' },
     { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
-    { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
+    
     ]
   }
 
   render() {
+    const myarr=this.cityList.filter((itm)=> itm.country==='India')
     return (
       <div id="main">
-        {/* Do not remove the main div */}
+        <ol>
+          {myarr.map((itm,idx)=>(
+            <li key={`location${idx+1}`}>{`itm.name(${itm.country})`}</li>
+          ))}
+        </ol>
       </div>
     )
   }
